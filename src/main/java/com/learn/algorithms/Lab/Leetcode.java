61b51084f4c5a71be4920a811732b7a2431a9965
yossi78 <yossi78@gmail.com>
2026-06-28 08:33:19 +0300

    Add task manager

---
+++ src/main/java/com/learn/algorithms/Lab/Leetcode.java
@@ -0,0 +1,18 @@
+package com.learn.algorithms.Lab;
+
+public class Leetcode {
+
+
+
+
+    public int method1(){
+        return 5;
+    }
+
+
+    public static void main(String[] args) {
+        Leetcode l1 = new Leetcode();
+        int result = l1.method1();
+        System.out.println("result="+result);
+    }
+}
--- src/main/java/com/learn/algorithms/practice/taskManagment/TaskManager.java
+++ src/main/java/com/learn/algorithms/practice/taskManagment/TaskManager.java
@@ -26,21 +26,35 @@ public class TaskManager {
         return task;
     }
 
+
+
     public static void main(String[] args) throws InterruptedException {
         TaskManager t = new TaskManager();
-        t.addTask(33,1);
-        t.addTask(40,2);
-        t.addTask(11,1);
+        t.addTask(10,1);
         t.addTask(20,2);
-        t.addTask(22,1);
-        t.addTask(60,2);
+        t.addTask(11,1);
+        t.addTask(21,2);
+        t.addTask(12,1);
+        t.addTask(13,1);
+        t.addTask(14,1);
+        t.addTask(15,1);
+        t.addTask(16,1);
+        t.addTask(17,1);
+        t.addTask(18,1);
+        t.addTask(19,1);
+        t.addTask(22,2);
+        t.addTask(23,2);
+        t.addTask(24,2);
+        t.addTask(25,2);
+        t.addTask(30,3);
 
 
-        for(int i=0;i<6;i++) {
+        while(!t.pbq.isEmpty()) {
             Task task = t.getNextTask();
             System.out.println("next task = " + task.taskId);
             Thread.sleep(1000);
         }
+
 
 
     }
